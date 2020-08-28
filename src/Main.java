ba128bfa8d0da66b23b1dfc20da93284c71d98e1
piprap <emil-rere@live.dk>
2020-08-28 10:20:11 +0200

    update

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hvad er joke");
+        System.out.println("Hvorfor blev blondinen glad for at samle et pusslespil på 6 måneder");
+        System.out.println("Fordi der stod 2-4år");
     }
 }
