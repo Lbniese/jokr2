1fa555242503680b28291cd8e9fc087332d9360b
Lbniese <lbniese@gmail.com>
2020-08-28 11:22:16 +0200

    Version 3
    Fetch joke from Jokes API

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,19 @@
-import java.util.Random;
+import java.io.IOException;
+import java.net.URL;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         boolean isRunning = true;
         Scanner console = new Scanner(System.in);
-
-        String[] jokes = {"Hvorfor blev blondinen glad for at samle et pusslespil på 6 måneder? Fordi der stod 2-4 år", "My new thesaurus is terrible. In fact, it's so bad, I'd say it's terrible.", "How are false teeth like stars? They come out at night!", "Why are skeletons so calm? Because nothing gets under their skin.", "I got fired from a florist, apparently I took too many leaves.", "Q: What’s 50 Cent’s name in Zimbabwe? A: 200 Dollars."};
-
-
-        Random r = new Random();
-        int randomNumber = r.nextInt(jokes.length);
-
 
         while (isRunning) {
             String input = console.nextLine();
             if (input != null) {
-                System.out.println(jokes[randomNumber]);
-                randomNumber = r.nextInt(jokes.length);
+                String jokesUrl = new Scanner(new URL("https://sv443.net/jokeapi/v2/joke/Any?format=txt").openStream(), "UTF-8").useDelimiter("\\A").next();
+
+                System.out.println(jokesUrl);
+
             }
         }
     }
