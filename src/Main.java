576197fe6ba0d3d4169fc602bbb038fba3205ca7
Lbniese <lbniese@gmail.com>
2020-08-28 10:42:30 +0200

    Version 2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,24 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hvorfor blev blondinen glad for at samle et pusslespil på 6 måneder");
-        System.out.println("Fordi der stod 2-4år");
+        boolean isRunning = true;
+        Scanner console = new Scanner(System.in);
+
+        String[] jokes = {"My new thesaurus is terrible. In fact, it's so bad, I'd say it's terrible.", "How are false teeth like stars? They come out at night!", "Why are skeletons so calm? Because nothing gets under their skin.", "I got fired from a florist, apparently I took too many leaves.", "Q: What’s 50 Cent’s name in Zimbabwe? A: 200 Dollars."};
+
+
+        Random r = new Random();
+        int randomNumber = r.nextInt(jokes.length);
+
+
+        while (isRunning) {
+            String input = console.nextLine();
+            if (input != null) {
+                System.out.println(jokes[randomNumber]);
+                randomNumber = r.nextInt(jokes.length);
+            }
+        }
     }
 }
